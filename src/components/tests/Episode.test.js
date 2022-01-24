06ce2fccc9cc78b0d1f63d197c725df2fc36c6e3
Hailey Magoline <magoline.hailey@gmail.com>
2022-01-24 15:43:47 -0600

    added first test for episode.test.js

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,9 +3,30 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
+const testEpisode = {
+    id: 0,
+    image: 'string',
+    name: 'name',
+    season: 0,
+    number: 0,
+    summary: 'summary',
+    runtime: 0,
+  }
 
-test("renders without error", () => {});
+test("renders without error", () => {
 
-test("renders the summary test passed as prop", ()=>{});
+    render(<Episode episode={testEpisode} />);
 
-test("renders default image when image is not defined", ()=>{});
+});
+
+
+test("renders the summary test passed as prop", ()=>{
+
+    render(<Episode episode={testEpisode.summary}/>);
+
+});
+
+
+test("renders default image when image is not defined", ()=>{
+
+});

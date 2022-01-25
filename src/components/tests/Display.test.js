9a8ce8552e79e415f46db2b1bebd278e8be5d07b
Hailey Magoline <magoline.hailey@gmail.com>
2022-01-24 23:31:08 -0600

    finished display.test.js

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -4,12 +4,60 @@ import { render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Display from './../Display';
+import mockFetchShow from './../../api/fetchShow';
 
+const testShow = {
+    name: 'show name',
+    summary: 'show summary',
+    seasons: [
+            {
+            id: 1,
+            name: 'season 1',
+            episodes: []
+            },
+
+            {
+                id: 2,
+                name: 'season 2',
+                episodes: []
+            },
+
+            {
+                id: 3,
+                name: 'season 3',
+                episodes: []
+            },
+
+            {
+                id: 4,
+                name: 'season 4',
+                episodes: []
+            },
+    ],
+}
 
 test('renders without errors with no props', ()=>{
-
+    render(<Display />);
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test('renders Show component when the button is clicked ', async ()=>{
+    mockFetchShow.mockResolvedValueOnce(testShow);
+    render(<Display/>) 
+    const button = screen.getByText(/Press to Get Show Data/i);
+    userEvent.click(button);
+    const show = screen.findByTestId("show-container");
+    expect(show).toBeInTheDocument();
+});
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+test('renders show season options matching your data when the button is clicked', async () => {
+    mockFetchShow.mockResolvedValueOnce(testShow)
+    render(<Display/>) 
+    const button = screen.getByRole(button);
+    userEvent.click(button);
+
+    await waitFor(()=> {
+        const options = screen.queryAllByTestId('season-option') 
+        expect(options).toHaveLength(4);
+    });
+
+});

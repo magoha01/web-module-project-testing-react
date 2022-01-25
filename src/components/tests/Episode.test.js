a828159d1fb71626a958c9cc03bec1a1a03398a3
Hailey Magoline <magoline.hailey@gmail.com>
2022-01-24 18:25:25 -0600

    finished episode tests git add .

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, screen } from '@testing-library/react';
+import { queryByAltText, render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
 const testEpisode = {
     id: 0,
-    image: 'string',
+    image: 'https://i.ibb.co/2FsfXqM/stranger-things.png',
     name: 'name',
     season: 0,
     number: 0,
-    summary: 'summary',
+    summary: 'A specific Summary Statement.',
+    runtime: 0,
+  }
+
+  const imgTestEpisode = {
+    id: 0,
+    image: null,
+    name: 'name',
+    season: 0,
+    number: 0,
+    summary: 'A specific Summary Statement.',
     runtime: 0,
   }
 
@@ -22,11 +32,26 @@ test("renders without error", () => {
 
 test("renders the summary test passed as prop", ()=>{
 
-    render(<Episode episode={testEpisode.summary}/>);
+    render(<Episode episode={testEpisode}/>);
+
+    const summaryText = screen.queryByText(/A specific Summary Statement./i);
+
+    expect(summaryText).toBeInTheDocument();
+    expect(summaryText).toHaveTextContent(/A specific Summary Statement./i);
+    expect(summaryText).toBeTruthy();
+    expect(summaryText).toBeVisible();
 
 });
 
 
 test("renders default image when image is not defined", ()=>{
+
+    render(<Episode episode={imgTestEpisode}/>);
+
+    const episodeImgAlt = screen.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png');
+
+    expect(episodeImgAlt).toBeInTheDocument();
+    expect(episodeImgAlt).toBeTruthy();
+  
 
 });
